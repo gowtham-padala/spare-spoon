000e3cb5abefce78ac0609f5d7882246721bff55
Faiyez <smfnoor@mun.ca>
2023-11-16 21:34:28 -0330

    fav-recipes-final

---
+++ code/lib/model/recipe_model.dart
@@ -0,0 +1,8 @@
+
+class RecipeModel {
+  final String name;
+  final String recipe;
+  bool favourite = false;
+
+  RecipeModel({required this.recipe, required this.name, required this.favourite});
+}

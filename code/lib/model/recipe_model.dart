6a1f3497428eecd4012bcfa32095be1c6737b2b1
Faiyez <smfnoor@mun.ca>
2023-11-19 02:17:56 -0330

    added missing files

---
+++ code/lib/model/recipe_model.dart
@@ -0,0 +1,7 @@
+
+class RecipeModel {
+  final String name;
+  final String recipe;
+  bool favourite = false;
+  RecipeModel({required this.recipe, required this.name, required this.favourite});
+}
